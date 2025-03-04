3edfe134339ac2c39a2c8d1ddcc05ec1a6c74b26
Karan Karan <Karan.Karan@Karan.local>
2025-03-04 16:36:01 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <nav className="navbar">
+          <input type="text" id='input1' className='input' placeholder='Search here for a task...'/>
+          <div className="nav-box">
+            <input type="text" id='input2' className='input'placeholder='Filter by Assignee'/>
+            <input type="text" id='input3' className='input' placeholder='Filter by Priority'/>
+            <button className='nav-button'>Add Task</button>
+          </div>
+        </nav>
+
+        
+        <div className="task-containers"></div>
+        <div className="task-containers"></div>
+        <div className="task-containers"></div>
+        <div className="task-containers"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
