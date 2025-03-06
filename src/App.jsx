cb9f293821765005b4988acec550ca0b3b6c23bf
Karan Karan <Karan.Karan@Karan.local>
2025-03-06 11:31:45 +0530

    appending the card into the container based on the status successfully implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,129 @@
-import TaskForm from './Components/TaskForm'
-import SearchIcon from '@mui/icons-material/Search';
-import './App.css'
-import { useState } from 'react';
-import ItemCard from './Components/ItemCard';
+import TaskForm from "./Components/TaskForm";
+import SearchIcon from "@mui/icons-material/Search";
+import "./App.css";
+import { useState } from "react";
+import ItemCard from "./Components/ItemCard";
 function App() {
-  const [data,setData] = useState([]);
-  const [showForm,setShowForm] = useState(false);
+  const [data, setData] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [search,setSearch] = useState({
+    searchByTask: '',
+    searchByAssignee: '',
+    searchByPriority: ''
+  });
 
-  const handleButtonClick = ()=>{
+  const handleButtonClick = () => {
     setShowForm(true);
-  }
-  
+  };
+
   return (
     <>
-      
-
       <div className="container">
         <nav className="navbar">
-          <div style={{display: 'flex',alignItems:'center',backgroundColor:'white',padding:'3px',borderRadius:'3px'}}>
-            <SearchIcon sx={{backgroundColor:'white',fontSize:'1.15rem'}}/>
-            <input type="text" id='input1' className='input' placeholder='Search here for a task...'/>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              backgroundColor: "white",
+              padding: "3px",
+              borderRadius: "3px",
+            }}
+          >
+            <SearchIcon
+              sx={{ backgroundColor: "white", fontSize: "1.15rem" }}
+            />
+            <input
+              type="text"
+              id="input1"
+              className="input"
+              placeholder="Search here for a task..."
+            />
           </div>
-          
-          
+
           <div className="nav-box">
-            <input type="text" id='input2' className='input'placeholder='Filter by Assignee'/>
-            <input type="text" id='input3' className='input' placeholder='Filter by Priority'/>
-            <button className='nav-button' onClick={handleButtonClick}>Add Task</button>
+            <input
+              type="text"
+              id="input2"
+              className="input"
+              placeholder="Filter by Assignee"
+            />
+            <input
+              type="text"
+              id="input3"
+              className="input"
+              placeholder="Filter by Priority"
+            />
+            <button className="nav-button" onClick={handleButtonClick}>
+              Add Task
+            </button>
           </div>
         </nav>
 
-        {showForm && <TaskForm showForm={showForm} setShowForm={setShowForm} data={data} setData={setData} />}
-        
+        {showForm && (
+          <TaskForm
+            showForm={showForm}
+            setShowForm={setShowForm}
+            data={data}
+            setData={setData}
+          />
+        )}
 
         <div className="second-container">
-
           <div className="task-boxes">
-            <div className="box-title" id="box-title1">Backlog</div>
+            <div className="box-title" id="box-title1">
+              Backlog
+            </div>
             <div className="box-items">
-              <ItemCard/>
-              <ItemCard/>
+              {data
+                .filter((item) => item.status === "BackLog")
+                .map((item) => {
+                  return <ItemCard key={item.title} item={item} />;
+                })}
             </div>
           </div>
 
           <div className="task-boxes">
-            <div className="box-title" id="box-title2">In Progress</div>
-            <div className="box-items"></div>
-          </div>
-
-          <div className="task-boxes">
-            <div className="box-title" id="box-title3">Review</div>
-            <div className="box-items"></div>
-          </div>
-
-          <div className="task-boxes">
-            <div className="box-title" id="box-title4">Complete</div>
+            <div className="box-title" id="box-title2">
+              In Progress
+            </div>
             <div className="box-items">
-              <ItemCard/>
-              <ItemCard/>
+              {data
+                .filter((item) => item.status === "In Progress")
+                .map((item) => {
+                  return <ItemCard key={item.title} item={item} />;
+                })}
             </div>
           </div>
 
+          <div className="task-boxes">
+            <div className="box-title" id="box-title3">
+              Review
+            </div>
+            <div className="box-items">
+              {data
+                .filter((item) => item.status === "Review")
+                .map((item) => {
+                  return <ItemCard key={item.title} item={item} />;
+                })}
+            </div>
+          </div>
+
+          <div className="task-boxes">
+            <div className="box-title" id="box-title4">
+              Complete
+            </div>
+            <div className="box-items">
+              {data
+                .filter((item) => item.status === "Complete")
+                .map((item) => {
+                  return <ItemCard key={item.title} item={item} />;
+                })}
+            </div>
+          </div>
         </div>
-
       </div>
-
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
